6bfb5418bc910245f7c496a907793d0490136263
pprathamesh <prathameshp@axioned.com>
2022-06-11 18:50:32 +0530

    feat: add change calendar functionality

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -3,7 +3,7 @@ import "./searchInput.css";
 
 export default function SearchInput({fetchInput, placeholder}) {
   return (
-      <div className="center-align mt-2">
+      <div className="center-align mt-3">
         <input
           type="text"
           className="form-control m-0"
